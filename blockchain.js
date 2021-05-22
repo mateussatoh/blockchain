a2f1e162380e7bfd382571c0010cdce0bc9e6285
Mateus Satoh <mateussatoh@gmail.com>
2021-05-22 19:28:20 -0300

    add: proof of work

--- blockchain.js
+++ blockchain.js
@@ -3,20 +3,55 @@ import SHA256 from "crypto-js/sha256.js";
 const chain = [];
 
 export const Blockchain = {
-  createBlock(previousHash, proof) {
-    const block = {
-      index: chain.length + 1,
-      timestamp: new Date(),
-      data: Math.random(),
-      proof: proof,
-      previous_hash: previousHash,
-    };
+  createBlock() {
+    if (this.getLastBlock() === 0) {
+      const block = {
+        index: chain.length + 1,
+        timestamp: new Date(),
+        data: Math.random(),
+        previous_hash: 0,
+      };
+      //saida nonce e hash
+      const minedBlock = this.proofOfWork(block);
+      chain.push(minedBlock);
+      return chain;
+    } else {
+      const block = {
+        index: chain.length + 1,
+        timestamp: new Date(),
+        data: Math.random(),
+        previous_hash: chain[this.getLastBlock() - 1].hash,
+      };
+      const minedBlock = this.proofOfWork(block);
+      chain.push(minedBlock);
+      return chain;
+    }
+  },
+  getLastBlock() {
+    return chain.length ? chain.length : 0;
+  },
 
-    chain.push(block);
+  proofOfWork(block) {
+    var isGoldenNonce = false;
+    var nonce = 0;
 
-    console.log("bloco", block);
-    console.log("chain", chain);
-
-    return block;
+    while (!isGoldenNonce) {
+      const blockHash = SHA256(JSON.stringify([block, nonce])).toString();
+      if (blockHash.substring(0, 3) === "000") {
+        console.log("Achei!!!", blockHash);
+        isGoldenNonce = true;
+        return {
+          index: block.index,
+          timestamp: block.timestamp,
+          data: block.data,
+          previous_hash: block.previous_hash,
+          nonce: nonce,
+          hash: blockHash,
+        };
+      } else {
+        isGoldenNonce = false;
+        nonce++;
+      }
+    }
   },
 };
--- server.js
+++ server.js
@@ -5,16 +5,21 @@ import { Blockchain } from "./blockchain.js";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({
+  res.send({
     error: "Wrong route",
     message: "The default mining route is  /mine",
   });
 });
 
 app.get("/mine", (req, res) => {
-  Blockchain.createBlock(0, 1);
-
-  //cria o bloco genese
+  res.status(200).json({
+    chain: Blockchain.createBlock(0, 1),
+  });
+});
+app.get("/last_block", (req, res) => {
+  res.status(200).json({
+    chain_size: Blockchain.getLastBlock(),
+  });
 });
 
 app.listen(3000, () => {
